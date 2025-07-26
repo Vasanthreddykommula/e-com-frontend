3f6a748452f11f58695d6432a840b9ffd8efce1a
= <=vasanthreddykommula@gmail.com>
2025-07-26 12:01:10 +0530

    styled registration form

--- index.js
+++ index.js
@@ -14,10 +14,20 @@ function handleClick(e){
     const path=e.target.pathname
     history.pushState(null,"",`${path}`)
     root.innerHTML=router[path]()
+
     
 }
 
 allAnchor.forEach((anchor)=>{
     anchor.addEventListener("click",handleClick)
+})
+
+window.addEventListener("popstate",(e)=>{
+    let path=location.pathname
+    if(path=="/index.html"){
+        root.innerHTML=" "
+    }else{
+        root.innerHTML=router[path]()
+    }
 })
 
